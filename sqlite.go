5f2a2741e284c9c8b443a5d68b8f116653b16c1d
su_lf <152139@qq.com>
2024-04-30 10:55:22 +0800

    Update go-sqlcipher

--- sqlite.go
+++ sqlite.go
@@ -5,7 +5,7 @@ import (
 	"strconv"
 	"strings"
 
-	_ "github.com/adon68/go-sqlcipher/v4"
+	_ "github.com/mutecomm/go-sqlcipher/v4"
 	"gorm.io/gorm"
 	"gorm.io/gorm/callbacks"
 	"gorm.io/gorm/clause"
